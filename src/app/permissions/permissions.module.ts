2ab442836a95bf44647c939985b45e2a6acb4d5c
franzmueller <franzmueller@users.noreply.github.com>
2019-06-20 11:00:49 +0200

    revert changes to permissions.module.ts

--- src/app/permissions/permissions.module.ts
+++ src/app/permissions/permissions.module.ts
@@ -49,6 +49,11 @@ const routes: Routes = [
         path: '',
         component: PermissionsListComponent,
         canActivate: [ValidTokenGuard]
+      },
+      {
+          path: 'edit',
+          component: PermissionsEditComponent,
+          canActivate: [ValidTokenGuard]
       }
     ]
   }
